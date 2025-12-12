2feeae05d8a0f42992d97b5bd09746063bb09388
MohammadLashkari <moe.lashkari@gmail.com>
2025-12-12 18:01:49 +0330

    first complete run

--- src/common.h
+++ src/common.h
@@ -1,42 +1,53 @@
+
 #ifndef COMMON_H
 #define COMMON_H
 
-struct flow_attribute
-{
-    s64 num_packet;        // Number of packets
-    s64 min_packet_length; // Minimum length of a packet
-    s64 max_packet_length; // Maximum length of a packet
-    s64 max_duration;      // Maximum time between two packets sent in the forward direction
-    s64 dst_port;          // Destination port
-    s64 header_length;     // TCP total header length
+#include <bpf/bpf_endian.h>
+#include <bpf/bpf_helpers.h>
+#include <vmlinux.h>
 
-    s64 last_packet_time;
-    s64 status;
-    u64 total_feature_extraction_time;
-    u64 detection_start_time;
+/* -----------------------------
+ * Flow attribute structure
+ * ----------------------------- */
+struct flow_attribute {
+	__s64 num_packet;
+	__s64 min_packet_length;
+	__s64 max_packet_length;
+	__s64 max_duration;
+	__s64 dst_port;
+	__s64 header_length;
 
-    s32 nn_idx;
-    s32 hidden1[32];
-    s32 hidden2[32];
+	__s64 last_packet_time;
+	__s64 status;
+
+	__u64 total_feature_extraction_time;
+	__u64 detection_start_time;
+
+	__s32 nn_idx;
+	__s32 hidden1[32];
+	__s32 hidden2[32];
 };
 
-struct flow
-{
-    int saddr;
-    int sport;
-    int daddr;
-    int dport;
+/* -----------------------------
+ * Flow key (tuple)
+ * ----------------------------- */
+struct flow {
+	__u32 saddr;
+	__u32 sport;
+	__u32 daddr;
+	__u32 dport;
 };
 
+/* -----------------------------
+ * Flow map
+ * ----------------------------- */
 #define MAX_PACKET_REACORD 8192
 
-struct
-{
-    __uint(type, BPF_MAP_TYPE_HASH);
-    __uint(max_entries, MAX_PACKET_REACORD);
-    __type(key, struct flow);
-    __type(value, struct flow_attribute);
+struct {
+	__uint(type, BPF_MAP_TYPE_HASH);
+	__uint(max_entries, MAX_PACKET_REACORD);
+	__type(key, struct flow);
+	__type(value, struct flow_attribute);
 } flow_map SEC(".maps");
 
-
-#endif
+#endif /* COMMON_H */
